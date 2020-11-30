78726023257fb033e4fcade42b2b2140878aa76a
Pierre Brunelle <pbrunelle@bloomberg.net>
2020-11-30 15:57:24 -0500

    Update description of diff

--- toynet/diff.h
+++ toynet/diff.h
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <vector>
 #include <boost/numeric/ublas/matrix.hpp>
+#include <boost/numeric/ublas/vector.hpp>
 
 namespace toynet {
 
@@ -10,33 +11,46 @@ namespace toynet {
 //
 // Our toy network has `inputs` input units.
 // The goal is to learn f(x) = x[0] - x[1] + x[2] - x[3] ...
-// We use MSE as our loss function: loss = (y - y_hat)^2
-// We implement a fully connected FFN with `hidden` hidden layers, each of
-// which has `width` units; `hidden` can be 0.
+// We use the squared error as our loss function: loss = (y - y_hat)^2
+// We implement a fully connected feed forward network with `hidden` hidden
+// layers, each of which has `width` units; `hidden` can be 0.
 // All layers have a *linear* activation function (i.e. no activation function)
 // There are no bias terms.
 // There are no regularization terms.
+// We perform gradient descent (i.e. no SGD).
+// We perform simple weight update: `W' = W - lr * DW`.
+//
+// See `diff.md` for a complete description.
 struct DiffNumbers {
     DiffNumbers(int hidden, int width, int inputs);
 
+    // Deterministically initialize weights.
     void init_weights();
 
+    // Given a list of training examples `training_set`, each containing
+    // a vector of length `inputs`, compute the activations and the loss
+    // (forward pass) and the gradients on the weights and activations
+    // (backward pass).  Update fields `A`, `loss`, `DW` and `G`.
+    // This operation is idem-potent.
     void forward_backward(const std::vector<ublas::vector<double>>& training_set);
 
+    // Use the computed gradients `DW`, the current weights `W` and the
+    // learning rate `lr` to update `W`.  This method should be called
+    // exactly once after each call to forward_backward.
     void update_weights(double lr);
 
     std::string print() const;
 
-    // The weights of the FFN
+    // The weights of the FFN, stored as:
     // W[layer][unit index of previous layer][unit index of layer]
-    // Where layer = 0 is the layer closest to the input
+    // where layer = 0 is the layer closest to the input
     std::vector<ublas::matrix<double>> W;
 
-    // The gradients of the loss for a given example w.r.t. the weights
+    // The gradients of the loss for a training set w.r.t. the weights
     std::vector<ublas::matrix<double>> DW;
 
     // The pre-activations (since everything is linear in our network,
-    // there are no post-activations)
+    // there are no post-activations), stored as:
     // A[layer][unit]
     std::vector<ublas::vector<double>> A;
 
